81a257f0e45827144c5a28b88168e1d471ad6614
red <red.avtovo@gmail.com>
2024-12-08 21:51:20 +0100

    bake the config with the path

--- airang/run.sh
+++ airang/run.sh
@@ -2,10 +2,5 @@
 
 mkdir -p /share/ariang
 
-# Add symlink to /aria2/data from /share/araiang
-if [ ! -L /aria2/data ]; then
-    ln -s /share/ariang /aria2/data
-fi
-
 # Run aria2
 /aria2/start.sh "$@"
